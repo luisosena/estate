9891a972f38e104117e2fc4c6de1430b441c82bb
Luis Osena <135612008+luisosena@users.noreply.github.com>
2026-01-28 12:20:29 +0300

    added table07 component

--- resources/js/pages/tests.tsx
+++ resources/js/pages/tests.tsx
@@ -1,10 +1,11 @@
 import { SectionsTable } from "@/components/sections-table"
-import  Stats07  from "@/components/stats-07"
+import Table05 from "@/components/table-05"
 
 export default function SomePage() {
   return (
     <div className="p-6">
       <SectionsTable />
+      <Table05/>
     </div>
   )
 }
