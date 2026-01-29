5c341a4bcc84588f2184cad70ef9d583fc36e2e1
Luis Osena <135612008+luisosena@users.noreply.github.com>
2026-01-29 16:58:09 +0300

    made changes

--- resources/js/pages/tests2.tsx
+++ resources/js/pages/tests2.tsx
@@ -39,7 +39,6 @@ export default function Test() {
         </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
-      <SidebarRail />
     </Sidebar>
     <SidebarInset>
       <SidebarTrigger/>
