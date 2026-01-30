6dbbe85a6f357365fd26b64516c117cffc3516ea
Luis Osena <135612008+luisosena@users.noreply.github.com>
2026-01-30 14:48:07 +0300

    created the user table migration file

---
+++ database/migrations/2026_01_30_114540_create_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tenant_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('role', ['tenant', 'landlord', 'admin']);
+            $table->timestamp('last_login_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+};
