a148984875b9ac34d2002bc0abf7515939229e82
Luis Osena <135612008+luisosena@users.noreply.github.com>
2026-01-30 15:09:09 +0300

    created the utilities table migration

---
+++ database/migrations/2026_01_30_120843_create_utilities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('utilities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tenancy_id')->constrained()->cascadeOnDelete();
+            $table->enum('type', ['water', 'electricity', 'gas', 'internet', 'security']);
+            $table->decimal('amount', 10, 2);
+            $table->string('billing_period');
+            $table->enum('status', ['paid', 'unpaid']);
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('utilities');
+    }
+};
