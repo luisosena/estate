27ee82fe50954a51d1853ac66654574c52a53ad8
Luis Osena <135612008+luisosena@users.noreply.github.com>
2026-02-02 00:47:43 +0300

    successfully created a new user, automatically uploaded to the db

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -5,7 +5,6 @@ namespace App\Actions\Fortify;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use App\Concerns\PasswordValidationRules;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 
