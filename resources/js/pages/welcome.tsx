ed86016fb8bc986870ae17d90220af6831e0661e
Luis Osena <135612008+luisosena@users.noreply.github.com>
2026-02-02 22:33:22 +0300

    edited routes in welcome.php

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -9,6 +9,8 @@ export default function Example() {
     return (
         <>
             <Link href="/login">Log in</Link>
+            <Link href="/register">Register</Link>
+            <Link href={route('login')}>Log in</Link>
             <Link href={route('mail')}>Mail</Link>
             <Link onClick={handleLogout}>Mail</Link>
             <Link href={route('logout')}>Log out</Link>
