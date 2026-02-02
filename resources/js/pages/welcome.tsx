032805627f3159233731c542b1915887a6cafacf
Luis Osena <135612008+luisosena@users.noreply.github.com>
2026-02-02 22:31:25 +0300

    edited web.php and welcome.php

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,22 +1,17 @@
-import { Link, router } from "@inertiajs/react"
+import { Link, router } from '@inertiajs/react';
 
-import { dashboard, login, register, logout } from '@/routes';
-import { Button } from '@/components/ui/button'
-import { route } from "ziggy-js";
-
-import AnimatedText from "@/components/animated-text";
+import { route } from 'ziggy-js';
 
 export default function Example() {
-  const handleLogout = () => {
-    router.post(route('logout4'));
-  };
-  return (
-    <>
-      
-      <Link href="/tests2">Log in</Link>
-      <Link  href={route('mail')} >Mail</Link>
-      <Link  onClick={handleLogout} >Mail</Link>
-      <Link href={route('logout')}>Log out</Link>
-    </>
-  );
+    const handleLogout = () => {
+        router.post(route('logout4'));
+    };
+    return (
+        <>
+            <Link href="/login">Log in</Link>
+            <Link href={route('mail')}>Mail</Link>
+            <Link onClick={handleLogout}>Mail</Link>
+            <Link href={route('logout')}>Log out</Link>
+        </>
+    );
 }
