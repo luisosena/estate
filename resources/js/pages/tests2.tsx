842e5e1b7dc25cbe60ca2b64057c237172300076
Luis Osena <135612008+luisosena@users.noreply.github.com>
2026-02-02 23:57:29 +0300

    edited tests2.tsx

--- resources/js/pages/tests2.tsx
+++ resources/js/pages/tests2.tsx
@@ -1,54 +1,49 @@
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarRail,
-  SidebarGroup,
-  SidebarGroupLabel,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuItem,
-  SidebarMenuButton,
-  SidebarProvider,
-  SidebarInset,
-  SidebarTrigger,
-  SidebarGroupAction,
-  SidebarMenuSub,
-  SidebarMenuSubItem,
-  SidebarMenuSubButton,
-  SidebarMenuAction,
-} from "@/components/ui/sidebar"
-import { Link } from "@inertiajs/react"
-import { route } from "ziggy-js"
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarHeader,
+    SidebarInset,
+    SidebarMenu,
+    SidebarMenuItem,
+    SidebarProvider,
+    SidebarRail,
+    SidebarTrigger,
+} from '@/components/ui/sidebar';
+import { Link } from '@inertiajs/react';
+import { route } from 'ziggy-js';
 
 export default function Test() {
-  return (
-    <SidebarProvider>
-      <Sidebar>
-        <SidebarHeader />
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <span>Sukuna</span>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <span>Gojo</span>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <span>Itadori</span>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroup>
-        </SidebarContent>
-        <SidebarFooter />
-        <SidebarRail />
-      </Sidebar>
-      <SidebarInset>
-        <SidebarTrigger />
-        <Link  href={route('tenant.dashboard')}
-        >Mail</Link>
-      </SidebarInset>
-    </SidebarProvider>
-  )
+    return (
+        <SidebarProvider>
+            <Sidebar>
+                <SidebarHeader />
+                <SidebarContent>
+                    <SidebarGroup>
+                        <SidebarMenu>
+                            <SidebarMenuItem>
+                                <span>Sukuna</span>
+                            </SidebarMenuItem>
+                            <SidebarMenuItem>
+                                <span>Gojo</span>
+                            </SidebarMenuItem>
+                            <SidebarMenuItem>
+                                <span>Itadori</span>
+                            </SidebarMenuItem>
+                            <SidebarMenuItem>
+                                <Link href={route('mail')}>Mail</Link>
+                            </SidebarMenuItem>
+                        </SidebarMenu>
+                    </SidebarGroup>
+                </SidebarContent>
+                <SidebarFooter />
+                <SidebarRail />
+            </Sidebar>
+            <SidebarInset>
+                <SidebarTrigger />
+                <Link href={route('tenant.dashboard')}>Mail</Link>
+            </SidebarInset>
+        </SidebarProvider>
+    );
 }
