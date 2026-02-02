adaae6662fa4161530ea2d3e609b94f2223a9d10
Luis Osena <135612008+luisosena@users.noreply.github.com>
2026-02-02 23:55:47 +0300

    edited tests.tsx

--- resources/js/pages/tests.tsx
+++ resources/js/pages/tests.tsx
@@ -1,11 +1,13 @@
-import { SectionsTable } from "@/components/sections-table"
-import Table05 from "@/components/table-05"
+import { SectionsTable } from '@/components/sections-table';
+import Stats07 from '@/components/stats-07';
+import Table05 from '@/components/table-05';
 
 export default function SomePage() {
-  return (
-    <div className="p-6">
-      <SectionsTable />
-      <Table05 />
-    </div>
-  )
+    return (
+        <div className="p-6">
+            <SectionsTable />
+            <Stats07 />
+            <Table05 />
+        </div>
+    );
 }
