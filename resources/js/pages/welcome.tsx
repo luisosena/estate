09424444d5bc122c7909bf7f188bda37ccf52238
Luis Osena <135612008+luisosena@users.noreply.github.com>
2026-02-02 11:56:21 +0300

    made changes

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,33 +1,21 @@
-import { Link } from "@inertiajs/react"
+import { Link, router } from "@inertiajs/react"
 
-import { dashboard, login, register } from '@/routes';
-import { type SharedData } from '@/types';
+import { dashboard, login, register, logout } from '@/routes';
 import { Button } from '@/components/ui/button'
 import { route } from "ziggy-js";
 
 import AnimatedText from "@/components/animated-text";
 
 export default function Example() {
+  const handleLogout = () => {
+    router.post(route('logout'));
+  };
   return (
     <>
-      <div>
-        <AnimatedText
-          text="Welcome to the Future"
-          className="text-4xl font-bold absolute p-50"
-          animationType="letters"
-          staggerDelay={0.08}
-          duration={0.6}
-        />
-      </div>
-      <div>
-      <Link
-        href="/tests2"
-      >
-        Log in
-      </Link>
-      <Link  href={route('mail')}
-        >Mail</Link>
-      </div>
+      
+      <Link href="/tests2">Log in</Link>
+      <Link  href={route('mail')} >Mail</Link>
+      <Link  onClick={handleLogout} >Mail</Link>
     </>
   );
 }
