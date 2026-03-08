a83f843a8a06bb92c7880936a028fc7066f06808
Luis Osena <luisosena2@gmail.com>
2026-03-08 20:30:43 +0300

    user can login on the expo go app

--- config/database.php
+++ config/database.php
@@ -61,6 +61,12 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            // Debug logging
+            'dump' => [
+                'dump_binary_path' => env('DB_DUMP_BINARY_PATH', ''),
+                'use_single_transaction',
+                'timeout' => 60,
+            ],
         ],
 
         'mariadb' => [
