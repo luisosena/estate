c37faa6b0783df9a2c6f0ef4cbbc7254a915d623
Luis Osena <luisosena2@gmail.com>
2026-03-11 15:46:02 +0300

    fixed a bug

--- app/Http/Controllers/Api/Tenant/UtilitiesController.php
+++ app/Http/Controllers/Api/Tenant/UtilitiesController.php
@@ -4,19 +4,40 @@ namespace App\Http\Controllers\Api\Tenant;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Services\UtilityService;
 
 class UtilitiesController extends Controller
 {
+    protected UtilityService $utilityService;
+
+    public function __construct(UtilityService $utilityService)
+    {
+        $this->utilityService = $utilityService;
+    }
+
     public function index(Request $request)
     {
         $user = $request->user();
         $tenant = $user->tenant;
 
+        $utilities = $this->utilityService->getTenantUtilities($tenant);
+
         return response()->json([
             'tenant' => [
                 'id' => $tenant->id,
                 'full_name' => $tenant->full_name
-            ]
+            ],
+            'utilities' => $utilities->map(function ($utility) {
+                return [
+                    'id' => $utility->id,
+                    'type' => $utility->type,
+                    'amount' => $utility->amount,
+                    'due_date' => $utility->due_date,
+                    'status' => $utility->status,
+                    'reading' => $utility->reading,
+                    'period' => $utility->billing_period,
+                ];
+            })
         ]);
     }
 }
