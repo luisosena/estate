a4f0d11b538745a4fbc37c578773fdf823962e28
Luis Osena <luisosena2@gmail.com>
2026-05-10 15:56:20 +0300

    chore: delete dev artifact files and update .gitignore

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-use App\Models\User;
-
-$user = User::factory()->create([
-    'email_verified_at' => null,
-]);
-
-echo 'Has verified email: ' . ($user->hasVerifiedEmail() ? 'yes' : 'no') . PHP_EOL;
